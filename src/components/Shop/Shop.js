88aa9134b27e786d62864d3720b63715b5ddced4
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-03-13 20:20:50 +0600

    5 Sync state in multiple routes, calculate total price in review page added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import fakeData from '../../fakeData';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
-import {addToDatabaseCart} from '../../utilities/databaseManager';
+import {addToDatabaseCart, getDatabaseCart} from '../../utilities/databaseManager';
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
     const [cart,setCart] =useState([]);
 
+    useEffect(()=>{
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+        const previousCart = productKeys.map(existingKey =>{
+            const product = fakeData.find( pd => pd.key === existingKey)
+            product.quantity = savedCart[existingKey];
+            return product;
+        })
+        setCart(previousCart);
+    },[])
     const handleAddProduct = (product) => {
         const toBeAddedKey = product.key;
         const sameProduct = cart.find(pd => pd.key === toBeAddedKey);
