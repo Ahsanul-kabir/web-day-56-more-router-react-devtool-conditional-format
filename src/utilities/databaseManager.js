b07089d57918a161f7852c5389b63ee6748ed325
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-03-13 21:52:42 +0600

    6 Please Order, Clear state, show conditional image added

--- src/utilities/databaseManager.js
+++ src/utilities/databaseManager.js
@@ -1,7 +1,7 @@
 const getUser = () => {
     const existingUser = sessionStorage.getItem('userId');
     if (existingUser) {
-        return existingUser; 
+        return existingUser;
     } else {
         const newUser = 'user-' + new Date().getTime();
         sessionStorage.setItem('userId', newUser)
