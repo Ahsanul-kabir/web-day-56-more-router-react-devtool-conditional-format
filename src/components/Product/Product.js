e8246c752d8ba9cad97e24ceb83a9c7e74e0a4db
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-03-16 23:15:22 +0600

    7 Shortcut destructuring of props added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,9 +5,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
 
-const Product = (props) => {
+const Product = ({ product,showAddToCart, handleAddProduct }) => {
     // console.log(props);
-    const { img, name, seller, price, stock,key } = props.product;
+    // const { product,showAddToCart, handleAddProduct } = props
+    const { img, name, seller, price, stock, key } = product;
     return (
         <div className="product">
             <div>
@@ -15,13 +16,13 @@ const Product = (props) => {
                 <img src={img} alt="" />
             </div>
             <div>
-                <h3 className="product-name"><Link to={'/product/'+key}>{name}</Link></h3>
+                <h3 className="product-name"><Link to={'/product/' + key}>{name}</Link></h3>
                 <br />
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
                 <br />
                 <p><small>only {stock} left in stock - order soon</small></p>
-                {props.showAddToCart === true && <button className='main-button' onClick={() => props.handleAddProduct(props.product)}
+                {showAddToCart === true && <button className='main-button' onClick={() => handleAddProduct(product)}
                 ><FontAwesomeIcon icon={faShoppingCart} /> add to card</button>}
             </div>
         </div>
